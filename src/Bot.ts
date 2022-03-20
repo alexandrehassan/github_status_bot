803682fc25987ca15fc3ec5a68509bbddd31bf23
Alexandre Hassan <alexhassan@live.ca>
2022-03-20 13:45:46 -0400

    .env setup

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,7 +2,9 @@ import { Client, ClientOptions } from "discord.js";
 import interactionCreate from "./listeners/interactionCreate";
 import ready from "./listeners/ready";
 
-const token = ""; // add your token here
+require('dotenv').config()
+const token = process.env.BOT_TOKEN;
+const channel = process.env.GH_CHANNEL;
 
 console.log("Bot is starting...");
 
