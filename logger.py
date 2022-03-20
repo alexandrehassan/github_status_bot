a9d1afe42afa2a386747a345709ebee153ad936d
Alexandre Hassan <alexhassan@live.ca>
2022-03-20 13:35:28 -0400

    Working base ts bot

--- logger.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-LOG_LEVEL = "INFO"
-
-
-def log(level: str, message: str) -> None:
-    if level == LOG_LEVEL:
-        timeStamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"{timeStamp} - [{level}] - {message}")
